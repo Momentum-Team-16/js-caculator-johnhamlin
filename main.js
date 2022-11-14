c00c20c057428880428a134cf63facc47e02fbe1
John Hamlin <johnhamlin@gmail.com>
2022-11-14 17:54:31 -0500

    Add basic functionality. TODO: Handle errors, add mult and subtraction

--- main.js
+++ main.js
@@ -1,1 +1,61 @@
 'use strict';
+
+const OPERATORS = {
+  x: '*',
+  X: '*',
+  '–': '-',
+  '-': '-',
+  '+': '+',
+  '÷': '/',
+  '/': '/',
+};
+
+// array and event listeners for buttons
+const buttons = Array.from(document.querySelectorAll('.btn'));
+buttons.map(button => button.addEventListener('click', handleClicks));
+
+let displayText = '';
+// console.log((str += ' +'));
+// console.log((str += ' 1'));
+// console.log(math.evaluate(str));
+
+function handleClicks(event) {
+  let text = event.target.innerText;
+  if (text === 'C') {
+    clearDisplay();
+    return;
+  }
+  if (text === '=') {
+    let result = calculateResult(displayText);
+    clearDisplay();
+    updateDisplay(result);
+    return;
+  }
+
+  // all buttons except clear and equals
+  updateDisplay(text);
+}
+
+// initialize display
+function updateDisplay(text) {
+  displayText += text;
+  renderDisplay();
+}
+
+function clearDisplay() {
+  displayText = '';
+  renderDisplay();
+}
+
+function renderDisplay() {
+  let displayNode = document.querySelector('.display');
+  displayNode.innerText = displayText;
+}
+
+function calculateResult(text) {
+  // TODO: Handle Errors
+  return math.evaluate(text);
+}
+
+// MAIN
+clearDisplay();
